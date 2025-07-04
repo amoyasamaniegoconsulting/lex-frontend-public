8171866ff408ac3b46b8c7033e5249f31e3dcf14
amoyasamaniegoconsulting <amoya@samaniegoconsulting.com>
2025-07-04 13:11:34 -0500

    Fix FastApi V2

--- api/index.py
+++ api/index.py
@@ -2,19 +2,20 @@ from fastapi import FastAPI
 from api.routers import flowEdge, flowNode, process, data, balances, flow, typeNode
 
 
+
 ### Create FastAPI instance with custom docs and openapi url
-app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
 # Rutas
 # Registra las rutas
-app.include_router(process.router, prefix="/api/v1/py", tags=["Process"])
-app.include_router(flowNode.router, prefix="/api/v1/py", tags=["FlowNode"])
-app.include_router(flowEdge.router, prefix="/api/v1/py", tags=["FlowEdge"])
-app.include_router(typeNode.router, prefix="/api/v1/py", tags=["TypeNode"])
-app.include_router(data.router, prefix="/api/v1/py", tags=["Data"])
-app.include_router(balances.router, prefix="/api/v1/py", tags=["Balances"])
-app.include_router(flow.router, prefix="/api/v1/py", tags=["Flow"])
-       
+app.include_router(process.router, prefix="/api", tags=["Process"])
+app.include_router(flowNode.router, prefix="/api", tags=["FlowNode"])
+app.include_router(flowEdge.router, prefix="/api", tags=["FlowEdge"])
+app.include_router(typeNode.router, prefix="/api", tags=["TypeNode"])
+app.include_router(data.router, prefix="/api", tags=["Data"])
+app.include_router(balances.router, prefix="/api", tags=["Balances"])
+app.include_router(flow.router, prefix="/api", tags=["Flow"])
+        
        
 # Functional Method
 @app.get("/helloFastApi")
